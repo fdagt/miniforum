8355010ce54326940ac6f874a5feca83dada28e2
fdagt <112229551+fdagt@users.noreply.github.com>
2023-04-26 14:00:25 +0900

    スレッド管理機能を追加

--- miniforum/admin.py
+++ miniforum/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Thread, Post
+
+class PostInline(admin.StackedInline):
+    model = Post
+    extra = 1
+    
+class ThreadAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at', 'updated_at']
+    list_filter = ['created_at', 'updated_at']
+    search_fields = ['title']
+    inlines = [PostInline]
+    
+admin.site.register(Thread, ThreadAdmin)
